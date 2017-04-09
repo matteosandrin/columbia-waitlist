32edc81f8c30786bf20b9d53b95c9a1f97dde0a7
matteosandrin <matteo.sandrin@hotmail.it>
2017-04-09 08:38:44 +0200

    Start taxi data visualisation Correct displaying issues in high aspect-ratio monitors

---
+++ post-data/visualizing-taxis/taxis.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGVvc2FuZHJpbiIsImEiOiJjajE5dHFrNTgwMDY5MnFxbXBldzA2aTliIn0.KHzhRZCopAziY_O0CJxPPw';
+var map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/dark-v9',
+		zoom: 12,
+		center: [-73.983858,40.7497247],
+		maxBounds: [[-74.2495941,40.5765633],[-73.4797139,41.0469793]]	
+		});
+
