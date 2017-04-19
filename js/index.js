36f584c1c945d004c37496146de9712aeba8de6b
matteosandrin <matteo.sandrin@hotmail.it>
2017-04-19 18:43:08 +0200

    Integrate taxi visualisation into slide structure Create new "care" section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#20457C'],
-		anchors: ['welcome', 'data', 'columbia', 'photography', 'attend'],
+		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'black', 'whitesmoke', '#B42A43', '#B42A43','#20457C'],
+		anchors: ['welcome', 'data', 'columbia', 'taxis','photography', 'travel', 'site', 'attend'],
 		scrollingSpeed: 500,
 		navigation: true,
 		autoScrolling: false,
-		parallax: true
+		onLeave: function (index, nextIndex, direction) {
+			if (index == 3 && nextIndex == 4 && $(".replay-button").text() == "Play" && $(".replay-button").css("opacity") == 1) { // the next slide is the taxi visualisation
+				$(".replay-button").click();
+			}
+			console.log("going from section " + index + " to section " + nextIndex);
+		}
 	});
 
 });
@@ -15,4 +20,9 @@ window.addEventListener('keydown', function (key) {
 		case 38: $.fn.fullpage.moveSectionUp();		break; //arrow up
 		case 40: $.fn.fullpage.moveSectionDown();	break; //arrow down
 	}
+});
+
+$(document).ready(function(){
+	$('.graph-frame').load("post-data/visualizing-students/connecting-classes-graph.html");
+	$('.taxis-map-wrapper').load("post-data/visualizing-taxis/taxis.html");
 });
--- post-data/visualizing-taxis/taxis-columbia.js
+++ post-data/visualizing-taxis/taxis-columbia.js
@@ -1,45 +1,62 @@
-// also show a tally of th number of pickups and dropoffs
+var bounds_poly = [[-73.949313, 40.807673],
+				   [-73.965538, 40.814000],
+				   [-73.971439, 40.807532],
+				   [-73.954771, 40.800006]]
+var sw_corner = [-73.968180, 40.800455]; // [longitude, latitude]
+var ne_corner = [-73.951700, 40.813548]; // [longitude, latitude]
 
-var sw_corner = [-73.980107, 40.799757]; // [longitude, latitude]
-var ne_corner = [-73.943775, 40.815293]; // [longitude, latitude]
 var columbia_center = [-73.961906,40.808210];
 var columbia_map_bounds = [sw_corner,ne_corner];
-var real_seconds_per_frame = 5;
-var delta = 60 * 60 / real_seconds_per_frame;
-var delete_after_frames = 25;
-var dot_radius = 15;
+
+var pickup_color = '#D73224';
+var dropoff_color = '#3778FA';
+var tally = {
+	pickup: 0,
+	dropoff: 0
+};
+
+var real_seconds_per_frame = 20;
+var delta = 2 * 60 * 60 / real_seconds_per_frame;
+var delete_after_frames = 5;
+var dot_radius = 10;
 var now = moment().year(2015);
 var then = moment().year(2015).add(delta * real_seconds_per_frame, 'seconds');
 var realTime = moment(now.valueOf());
+
 $(".display-date").text(realTime.format("MMMM Do YYYY"));
 $(".display-time").text(realTime.format("h:mm a"));
 $(".display-status").text("Downloading tiles from Mapbox...");
 
 var frames = Array.apply(null, Array(delta));
-var activePoints = [];
 
 point = {
-			"type": "FeatureCollection",
-			"features": [{
-				"type": "Feature",
-				"geometry": {
-					"type": "Point",
-					"coordinates": []
-				}
-			}]
-		};
+	"type": "FeatureCollection",
+	"features": [{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": []
+		}
+	}]
+};
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGVvc2FuZHJpbiIsImEiOiJjajE5dHFrNTgwMDY5MnFxbXBldzA2aTliIn0.KHzhRZCopAziY_O0CJxPPw';
+
 var map = new mapboxgl.Map({
 		container: 'map',
 		style: 'mapbox://styles/mapbox/dark-v9',
-		zoom: 12,
+		zoom: 16,
 		center: columbia_center,
-		maxBounds: columbia_map_bounds,
-		interactive: false
+		interactive: false,
+		bearing: 29.1
 		});
 
 map.on('load', function () {
+
+	$(".mapboxgl-canvas").css('width',"100%");
+	$(".mapboxgl-canvas").css('height',"100%");
+
+	map.resize();
 
 	$(".display-status").text("Downloading taxi trips from NYC Open Data...");
 
@@ -61,7 +78,6 @@ map.on('load', function () {
 	  .withDataset('2yzn-sicd')
 	  .where(where)
 	  .order('dropoff_datetime ASC')
-	  // .limit()
 	  .getRows()
 		.on('success', drawRows)
 		.on('error', function(error) { console.error(error); });
@@ -70,11 +86,11 @@ map.on('load', function () {
 
 function drawRows(rows){
 
-	console.log(rows);
 	console.log("Total # of taxis: " + rows.length);
-	rows.forEach(function (row, index) {
 
-		// $(".display-status").text("Crunching taxi trip " + (index + 1));
+	processLargeArrayAsync(rows, processTaxi);
+
+	function processTaxi (row, index) {
 		
 		var pickup_pos = [row.pickup_longitude, row.pickup_latitude];
 		var dropoff_pos = [row.dropoff_longitude, row.dropoff_latitude];
@@ -86,9 +102,6 @@ function drawRows(rows){
 			var two_millis = two.valueOf();
 			var one_millis = one.valueOf();
 
-			console.log(one);
-			console.log(two);
-
 			return two_millis - one_millis;
 		}
 
@@ -98,90 +111,75 @@ function drawRows(rows){
 
 			if (frame_to_delete_index >= delta) { frame_to_delete_index = delta - 1 };
 
-			for (var i = 0; i < delete_after_frames; i++) {
-				
-				var frame_to_change_index = frame_index + i;
-				var new_radius = dot_radius * (1 - i / delete_after_frames);
-
-				if (frames[frame_to_change_index] == null) { frames[frame_to_change_index] = [];};
-				frames[frame_to_change_index].push({
-					kind: 'change',
-					id_num: id_num,
-					radius: new_radius,
-					color: color
-				});
-
-			};
-
 			if (frames[frame_to_delete_index] == null) { frames[frame_to_delete_index] = [];};
 			frames[frame_to_delete_index].push({
 				kind: 'delete',
-				id_num: id_num,
-				color: color
+				id_num: id_num
 			});
 
 		}
 
 		function addFrame(position,time,kind) {
 			
-			var color = '#FF0';
-			if (kind == 'dropoff') { color = '#F00'};
+			var color = pickup_color;
+			if (kind == 'dropoff') { color = dropoff_color };
 			var spread = calcSpread(now,moment(time));
 			var id_num = makeid(8);
+			// var within_bounds = position[0] > sw_corner [0] && position[0] < ne_corner[0] && position[1] > sw_corner [1] && position[1] < ne_corner[1];
+			var within_bounds = inside(position,bounds_poly);
 
-			if (spread > 0 && spread <= delta * real_seconds_per_frame * 1000) {
+			if (spread > 0 && spread <= delta * real_seconds_per_frame * 1000 && within_bounds) {
 				
 				var frame_index = Math.round(Math.round(spread / 1000) / real_seconds_per_frame);
-				console.log(frame_index);
+				
 				if (frames[frame_index] == null) { frames[frame_index] = [];};
 				frames[frame_index].push({
-					coordinates: position,
 					kind: kind,
 					id_num: id_num,
 					color: color
 				});
 
+				point.features[0].geometry.coordinates = position;
+
+				map.addSource(id_num, {
+					"type": "geojson",
+					"data": point
+				});
+
+				map.addLayer({
+					"id": id_num,
+					"source": id_num,
+					"type": "circle",
+					"paint": {
+						"circle-radius": dot_radius,
+						"circle-color": color
+					},
+					"layout": {
+						"visibility": "none"
+					}
+				});
+
 				removeFrame(frame_index,id_num,color);
 
 			}
-		}
-
-		
+		}	
 
 		addFrame(pickup_pos,row.pickup_datetime,'pickup');
 		addFrame(dropoff_pos,row.dropoff_datetime,'dropoff');
-		
-		// map.addSource('point-'+index, {
-		//     "type": "geojson",
-		//     "data": taxi.point
-		// });
-
-		// map.addLayer({
-		//     "id": "point-" + index,
-		//     "source": "point-" + index,
-		//     "type": "circle",
-		//     "paint": {
-		//         "circle-radius": 5,
-		//         "circle-color": "#FF0"
-		//     }
-		// });
-
-		// requestAnimationFrame(taxi.animate);
-
-		console.log("loaded taxi #" + (index + 1));
 	
-	});
+	}
 
-	console.log(frames)
-
-	requestAnimationFrame(animate);
 	$(".loading-container").css('opacity',0);
 	$(".map-overlay").css('opacity',1);
+	$(".replay-button").text('Play');
+	$(".replay-button").css('opacity',1);
+	$(".replay-button").css('cursor','pointer');
+	$(".replay-button").removeAttr('disabled');
 
 }
 
 var counter = 0;
-var fps = 15;
+var fps = 10;
 var fps_now = 0;
 var fps_then = moment();
 var fps_interval = 1000/fps;
@@ -200,55 +198,31 @@ function animate() {
 
 			realTime.add(real_seconds_per_frame,'seconds');
 			$(".display-time").text(realTime.format("h:mm a"));
+			$(".display-pickups").text();
 
 			var frame = frames[counter];
 
 			if (frame != undefined) {
 
-				console.log("displaying frame: " + counter);
-				console.log("  # of events in frame: " + frame.length);
-
 				frame.forEach(function (e, index) {
 					if (e.kind == 'delete') {
 						try {
-							map.removeLayer(e.id_num);
-							map.removeSource(e.id_num);
+							map.setLayoutProperty(e.id_num,"visibility","none");
 						} catch(error) {
 							console.log("Failed to delete layer: " + e.id_num);
 						}
 					} else if (e.kind == 'pickup' || e.kind == 'dropoff') {
 
-						point.features[0].geometry.coordinates = e.coordinates;
+						tally[e.kind] += 1;
+						map.setPaintProperty(e.id_num, 'circle-radius', dot_radius);
+						map.setLayoutProperty(e.id_num,"visibility","visible");
 
-						map.addSource(e.id_num, {
-							"type": "geojson",
-							"data": point
-						});
-
-						map.addLayer({
-							"id": e.id_num,
-							"source": e.id_num,
-							"type": "circle",
-							"paint": {
-								"circle-radius": dot_radius,
-								"circle-color": e.color
-							}
-						});
-					} else if (e.kind == 'change') {
-
-						map.removeLayer(e.id_num);
-						map.addLayer({
-							"id": e.id_num,
-							"source": e.id_num,
-							"type": "circle",
-							"paint": {
-								"circle-radius": e.radius,
-								"circle-color": e.color
-							}
-						});
-					}
+					} 
 				});
 			}
+
+			$(".display-pickups").text(tally.pickup + " pickups");
+			$(".display-dropoffs").text(tally.dropoff + " dropoffs");
 
 			counter++;
 		} else {
@@ -265,6 +239,9 @@ function animate() {
 
 function makeid(len)
 {
+
+	// http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+
 	var text = "";
 	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
@@ -278,17 +255,53 @@ function replay() {
 	$(".replay-button").css('opacity',0);
 	$(".replay-button").css('cursor','initial');
 	$(".replay-button").prop('disabled', true);
+	$(".replay-button").text('Replay');
 	counter = 0;
+	tally.pickup = 0;
+	tally.dropoff = 0;
 	fps_then = moment();
 	realTime = moment(now.valueOf());
 	requestAnimationFrame(animate);
 }
 
+function inside(point, vs) {
+    
+    // http://stackoverflow.com/questions/22521982/js-check-if-point-inside-a-polygon
+    // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
 
+    var x = point[0], y = point[1];
 
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
 
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
 
+    return inside;
+}
 
+function processLargeArrayAsync(array, fn, chunk, context) {
 
+	// http://stackoverflow.com/questions/10344498/best-way-to-iterate-over-an-array-without-blocking-the-ui
 
-
+    context = context || window;
+    chunk = chunk || 10;
+    var index = 0;
+    function doChunk() {
+        var cnt = chunk;
+        while (cnt-- && index < array.length) {
+            // callback called with args (value, index, array)
+            fn.call(context, array[index], index);
+            ++index;
+        }
+        if (index < array.length) {
+            // set Timeout for async iteration
+            setTimeout(doChunk, 1);
+        }
+    }    
+    doChunk();    
+}
