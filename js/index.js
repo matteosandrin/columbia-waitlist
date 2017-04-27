2edf6ee0b7f4bb7ced78dc55023606927257abb9
matteosandrin <matteo.sandrin@hotmail.it>
2017-04-27 20:05:11 +0200

    Fix double notification bug

--- js/index.js
+++ js/index.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
 			if (index == 3 && nextIndex == 4 && $(".replay-button").text() == "Play" && $(".replay-button").css("opacity") == 1) { // the next slide is the taxi visualisation
 				$(".replay-button").click();
 			}
-			console.log("going from section " + index + " to section " + nextIndex);
 		}
 	});
 
@@ -23,6 +22,20 @@ window.addEventListener('keydown', function (key) {
 });
 
 $(document).ready(function(){
+	if (window.location.host.search("localhost") != -1) {
+		var payload = {
+			"app_id": "d920c173-d56e-43cd-bb87-fe8e9b461602",
+			"included_segments": ["All"],
+			"template_id": "5f5a37de-5579-4eec-ba8f-6252c50757cb"
+		}
+
+		var request = new XMLHttpRequest();
+		request.open('POST', "https://onesignal.com/api/v1/notifications", true);
+		request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+		request.setRequestHeader("Authorization", "Basic MjJhNzdlYWQtNzcyMS00OWEyLWJkNzUtMDljNDZmMzg1YzNm");
+		request.send(JSON.stringify(payload));
+		console.log("push");
+	}
 	$('.graph-frame').load("post-data/visualizing-students/connecting-classes-graph.html");
 	$('.taxis-map-wrapper').load("post-data/visualizing-taxis/taxis.html");
 });
